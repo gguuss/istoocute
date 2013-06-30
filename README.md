2dd9d355ea480dafbd44292f6cf2d6f689477f93
Gus Class <gguuss@gmail.com>
2013-06-30 16:31:55 -0700

    Shows better ways to render after sign-in

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ istoocute
 =========
 
 A demo site showing a few tricks for improving the sign in experience.
+
+To try the site out:
+
+# Step 1: Enable the Google+ API
+
+Create a [Google APIs Console project](https://code.google.com/apis/console),
+OAuth 2.0 client ID, enable the Google+ service, and register your JavaScript 
+origins.
+
+# Step 2. Set up the site
+
+Replace `YOUR_CLIENT_ID` in index.html with the client id you set up in Step 1.
+
+# Step 3. Start the site
+
+Either copy the folder to your web server or start a local web server. I 
+personally find the Python built-in server to be great for testing. To start
+it on port 8000, run:
+
+    python -m SimpleHTTPServer 8000
+
+# For more information
+
+* [Google+ JavaScript Quickstart](https://developers.google.com/+/quickstart/javascript)
---
+++ js/app.js
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+var helper = (function() {
+  return {
+    clientId :'YOUR_CLIENT_ID',
+    BASE_API_PATH: 'plus/v1/',
+    apiBase: '/api',
+    rootUrl: '',
+    authResult: '',
+    isFree: false,
+    user: '',
+    topRecipients: '',
+    mode: 'good',
+
+    /**
+     * Hides the sign in button and starts the post-authorization operations.
+     *
+     * @param {Object} authResult An Object which contains the access token and
+     *   other authentication information.
+     */
+    onSigninCallback: function(authResult) {
+        if (authResult['access_token']) {
+          console.log("Result from sign-in:");
+          console.log(authResult);
+
+          // Setup the base url for API calls
+          this.setBaseUrl();
+          this.authResult = authResult;
+
+          // Success.
+          // Hide the sign-in button
+          $('#gConnect').hide();
+
+          // Reflect the user profile
+          gapi.client.load('plus','v1', function(){
+            helper.connect();
+          });
+
+        } else if (authResult['error']) {
+          // You can handle various error conditions here
+        }
+    },
+
+    /**
+     * Calls the /api/connect endpoint to connect the user with the server.
+     *
+     * @param {Object} token An Object which contains the access token and
+     *   other authentication information.
+     */
+    connect: function(){
+      gapi.client.plus.people.get({userId:'me'}).execute(
+        function(result) {
+          helper.user = result;
+          $('#profileArea').hide();
+          $('#cuteMessage').hide();
+          
+          
+          if (helper.mode == 'good'){
+            $('#profileArea').html('Signed in as ' + helper.user.displayName + '!')
+          }else{
+            var html = helper.getProfileHTML(result);
+            $('#profileArea').html(html);
+          }
+
+          if (helper.mode == 'good' || helper.mode == 'better'){
+            $('#profileArea').css('margin-top', '0px');
+            $('#profileArea').show();
+          }
+          if (helper.mode == 'awesome'){
+            $('#profileArea').css('margin-top', '0px');
+            $('#profileArea' ).show();
+            $('#profileArea' ).effect( 'bounce', null, 500, null);
+          }
+          setTimeout("$('#disconnect').show()", 600);
+
+          $('#authButtons').show();
+        }
+      );
+    },
+
+    /**
+     * Gets HTML markup representing a user's profile
+     *
+     * @param {Object} The user object.
+     */
+    getProfileHTML: function(user){
+      var html = '<a target="_blank" href="' + user.url + '">' + '<img src="' +
+          user.image.url + '" alt="' + user.displayName + '" title="' +
+          user.displayName + '" height="35" />' + '</a> ' +  user.displayName;
+      return html;
+    },
+
+    /**
+     * Shows the modal for disconnecting an account.
+     */
+    showDisconnectDialog : function() {
+      $( "#disconnectDialog" ).dialog({
+        modal: true,
+        buttons: {
+          Disconnect: function(){
+            helper.disconnect();
+            $( this ).dialog( "close" );
+          },
+          Cancel: function(){
+            $( this ).dialog( "close" );
+          }
+        }
+      });
+    },
+
+    /**
+     * Calls the OAuth2 endpoint to disconnect the app for the user.
+     */
+    disconnect: function() {
+      // Revoke the access token.
+      $.ajax({
+        type: 'GET',
+        url: 'https://accounts.google.com/o/oauth2/revoke?token=' +
+            gapi.auth.getToken().access_token,
+        async: false,
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(result) {
+          console.log('revoke response: ' + result);
+          $('#authOps').hide();
+          $('#profileArea').empty();          
+          $('#authResult').empty();
+          $('#authButtons').hide();
+
+          $('#cuteMessage').show("slide", null, 250, null);
+          $('#gConnect').show();
+        },
+        error: function(e) {
+          console.log(e);
+        }
+      });
+    },
+
+    setBaseUrl: function(){
+      this.rootUrl = window.location.origin;
+    }
+  };
+})();
